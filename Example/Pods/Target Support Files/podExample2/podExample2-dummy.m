96b9b84093860feb80b816c1c6414968a137a5e9
Luis Alberto Mendoza Reyna <lmendore@everis.com>
2018-03-27 11:06:24 +0200

    first commit

---
+++ Example/Pods/Target Support Files/podExample2/podExample2-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_podExample2 : NSObject
+@end
+@implementation PodsDummy_podExample2
+@end
---
+++ podExample2/Classes/ClassExample.swift
@@ -0,0 +1,16 @@
+//
+//  ClassExample.swift
+//  podExample
+//
+//  Created by Luis Alberto Mendoza Reyna on 27/3/18.
+//
+
+import Foundation
+
+class ClassExample: NSObject {
+    
+    open func testingPod()
+    {
+        print("using pod...")
+    }
+}
